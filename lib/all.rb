b67d13dc1e21e0124b47cd6c8d167572bb6ce53f
sch1zo <andreas.eger.reg@googlemail.com>
2011-03-04 00:36:31 +0100

    damn typo
    Signed-off-by: sch1zo <andreas.eger.reg@googlemail.com>

--- lib/all.rb
+++ lib/all.rb
@@ -3,7 +3,7 @@ require 'redis'
 
 redis_config = if ENV['SCANTY_REDIS_URL']
 	require 'uri'
-	uri = URI.parse ENV['SCANTY_wREDIS_URL']
+	uri = URI.parse ENV['SCANTY_REDIS_URL']
 	{ :host => uri.host, :port => uri.port, :password => uri.password, :db => uri.path.gsub(/^\//, '') }
 else
 	{}
